3975d69fea8d5702e2890cc2b21ce080e28c5acd
Dimitris Dedousis <dimitris@jupiter.hades.net>
2015-03-11 03:31:58 +0200

    The way ngrams are created is changed.

--- src/ngrams/Tokenizer.java
+++ src/ngrams/Tokenizer.java
@@ -30,20 +30,25 @@ public class Tokenizer {
     private String europarlString;
     private int wordFrequency;
     private Map<String, Integer> dictionary;
+    private List<String> corpusWithTags;
+
     {
+        corpusWithTags = new ArrayList<>();
         ngramsList = new ArrayList<>();
         dictionary = new HashMap<>();
     }
+
     /**
-     * Number of grams is used to create the associated grams. For example 
-     * if you use the number (3) you create a trigram or if you use the number (2)
-     * you create a bigram etc. File to be read is the training file but also 
-     * it is used to extract words for the dictionary. Word frequency is used 
-     * for the dictionary creation. No word is accepted in the dictionary if it 
-     * has frequency lower than the given frequency, in the corpus.
+     * Number of grams is used to create the associated grams. For example if
+     * you use the number (3) you create a trigram or if you use the number (2)
+     * you create a bigram etc. File to be read is the training file but also it
+     * is used to extract words for the dictionary. Word frequency is used for
+     * the dictionary creation. No word is accepted in the dictionary if it has
+     * frequency lower than the given frequency, in the corpus.
+     *
      * @param numberOfNgrams
      * @param fileToBeRead
-     * @param wordFrequency 
+     * @param wordFrequency
      */
     public Tokenizer(int numberOfNgrams, File fileToBeRead, int wordFrequency) {
         this.numberOfNgrams = numberOfNgrams;
@@ -51,71 +56,119 @@ public class Tokenizer {
         this.wordFrequency = wordFrequency;
     }
 
+    /**
+     * I have no idea why this method is working! yet!
+     *
+     * @throws IOException
+     */
     public void startTokenization() throws IOException {
         europarlString = new String(Files.readAllBytes(fileToBeRead.toPath()));
         String replace = europarlString.replace("\n", " ");
         String replace1 = replace.replace("\r", " ");
         String replace2 = replace1.replace("\n\r", " ");
-        String replace3 = replace2.replaceAll("-", " ");
+        String replace3 = replace2.replaceAll("-", "");
         String replace8 = replace3.replace(",", "");
-        String[] split = replace8.split(" ");
+        String[] split1 = replace8.split(" ");
+        StringBuilder builder = generateStartTags(split1);
+        String tagedCorpus = builder.toString();
+        String replace4 = tagedCorpus.replace(".", "");
+        String replace5 = replace4.replace(",", "");
+        String replace6 = replace5.replace("'", "");
+        String replace7 = replace6.replace("\"", "");
+        String replace9 = replace7.replaceAll("  *", " ");
+        String[] split = replace9.split(" ");
         createDictionary(split);
-        printDictionary();
-        boolean isNewSentence = true;
+//        printDictionary();
+//        printTagedList();
         for (int i = numberOfNgrams - 1; i < split.length; i++) {
             String token = new String();
             int j = i;
-            if(dictionary.containsKey(split[i]) && dictionary.get(split[i]) > wordFrequency){
-            token = split[i];
-            }else{
+            if (dictionary.containsKey((String) split[i]) && dictionary.get((String) split[i]) > wordFrequency) {
+                token = (String) split[i];
+            } else {
                 token = "*unknown*";
             }
-                int k = j - numberOfNgrams + 1;
-            do  {
+            int k = j - numberOfNgrams + 1;
+            do {
                 j--;
-                if(dictionary.containsKey(split[j]) && dictionary.get(split[j]) > wordFrequency){
-                token = split[j] + " " + token;
-                }else{
-                token = "*unknown*" + " " + token;    
+                if (dictionary.containsKey((String) split[j]) && dictionary.get((String) split[j]) > wordFrequency) {
+                    token = split[j] + " " + token;
+                } else {
+                    token = "*unknown*" + " " + token;
                 }
-            }while(j - 1 != k - 1);
-            if(isNewSentence){
-                token = "<s>" + token;
-                isNewSentence = false;
-            }
-            if(token.endsWith(".") || token.endsWith("!") || token.endsWith("?")){
-                isNewSentence = true;
+            } while (j - 1 != k - 1);
+            if (split[ i + 1].equals("<s>")) {
                 i = i + numberOfNgrams - 1;
             }
-            String replace4 = token.replace(".", "");
-            String replace5 = replace4.replace(",", "");
-            String replace6 = replace5.replace("'", "");
-            String replace7 = replace6.replace("\"", "");
-            ngramsList.add(replace7);
-        }
-    }
-    
-    private void createDictionary(String[] splitedFile){
-        for(String word : splitedFile){
-            int wordFrequency = 1;
-            if(dictionary.containsKey(word)){
-             wordFrequency = dictionary.get(word) + 1;
-            }
-            dictionary.put(word, wordFrequency);
+            ngramsList.add(token);
         }
     }
 
-    private void printDictionary(){
-        for(Map.Entry<String, Integer> d : dictionary.entrySet()){
+    /**
+     * This method creates a dictionary of words from the corpus and stores the
+     * frequency of every word in the corpus.
+     *
+     * @param splitedFile
+     */
+    private void createDictionary(String[] splitedFile) {
+        for (String word : splitedFile) {
+            int wordFrequency = 1;
+            if (dictionary.containsKey(word)) {
+                wordFrequency = dictionary.get(word) + 1;
+            }
+            dictionary.put(word, wordFrequency);
+        }
+        dictionary.put("<s>", Integer.MAX_VALUE);
+    }
+
+    /**
+     * This method creates the <s> tags after the end of every sentence. The
+     * number of tags that is added to the start of every sentence is n-1 were n
+     * is the number of the grams that are to be created.
+     *
+     * @param file
+     * @return
+     */
+    private StringBuilder generateStartTags(String[] file) {
+        for (String token : file) {
+            corpusWithTags.add(token);
+            if (token.endsWith(".") || token.endsWith("!") || token.endsWith("?")) {
+                for (int i = 0; i < numberOfNgrams - 1; i++) {
+                    corpusWithTags.add("<s>");
+                }
+            }
+        }
+        StringBuilder b = new StringBuilder();
+        for (String s : corpusWithTags) {
+            b.append(s + " ");
+        }
+        return b;
+    }
+
+    /**
+     * This method prints all the tags that were added.
+     */
+    private void printTagedList() {
+        for (String word : corpusWithTags) {
+            System.out.println(word);
+        }
+    }
+
+    /**
+     * This method prints all the words in the dictionary with their frequency.
+     */
+    private void printDictionary() {
+        for (Map.Entry<String, Integer> d : dictionary.entrySet()) {
             System.out.println("Word " + d.getKey() + " has frequency " + d.getValue());
         }
     }
-    
+
     /**
      * This method does the same thing as the method startTokenization but it
-     * has the difference that it cant add the tag <s> at the beginning of every 
+     * has the difference that it cant add the tag <s> at the beginning of every
      * new sentence.
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public void startTokenizationWithLucene() throws IOException {
         europarlString = new String(Files.readAllBytes(fileToBeRead.toPath()));
